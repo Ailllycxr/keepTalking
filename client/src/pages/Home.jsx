d9a1206660cf0c2ff67fbf79f04de3baa4fdbd32
Xiaoran <xc2422@columbia.edu>
2023-09-13 17:45:19 -0700

    readme

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,6 +11,7 @@ export default function Home() {
 
   return (
     <>
+<<<<<<< HEAD
       <h1> Welcome to the Game</h1>
       <Row className={"justify-content-center"}>
         {state?.scoreSubmit && (
@@ -20,10 +21,36 @@ export default function Home() {
           </Alert>
         )}
         <LinkContainer to="/game">
+=======
+        <h1> Welcome to Keep Reacting and No One Gets Sued!</h1>
+        <Row className={'justify-content-center'}>
+        {state?.scoreSubmit && 
+          <Alert variant='success' style={{width: '50%'}}>
+              Your Score has been Saved! Head to the <Link to='/scores'>Scores tab</Link> to see how you ranked!
+          </Alert>}
+        </Row>
+        <Row className={'justify-content-center'}>
+          <LinkContainer to="/game">
+            <motion.button
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+              type="submit"
+              disabled={!Auth.loggedIn()}
+              className={'playButton'}
+            >
+              Play
+            </motion.button>
+          </LinkContainer>
+        </Row>
+        {!Auth.loggedIn() && 
+          <p>Please log in to play the game. Viewing the manual can be done by anyone, though!</p>}
+        <LinkContainer to="/manual">
+>>>>>>> 2da9584d8f04e5559433d349a53e750c5473f5bf
           <motion.button
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.8 }}
             type="submit"
+<<<<<<< HEAD
             disabled={!Auth.loggedIn()}
             className={"playButton"}
           >
@@ -35,6 +62,27 @@ export default function Home() {
         <p>
           Please log in to play the game. Viewing the manual can be done by
           anyone, though!
+=======
+            className={'manualButton'}
+          >
+            Manual
+          </motion.button>
+        </LinkContainer>
+        <p style={{textAlign: 'left'}}>Welcome to our little ripoff of <a href='https://keeptalkinggame.com/'>Keep Talking and Nobody Explodes,</a> made entirely in React! Here's how to play:<br/>
+          A round of the game has one driver and any number of teammates, each accessing this site via their own device. The game starts when the driver presses 'Play'.<br/>
+          On the game page, the driver will see a timer, a point counter, and several puzzles. While each puzzle exists, the points counter will increase based on its
+          Triage level: <br/>
+          <span style={{backgroundColor: 'red', color: 'black'}}>Red puzzles are your highest priority</span><br/>
+          <span style={{backgroundColor: 'yellow', color: 'black'}}>Yellow puzzles are your second highest priority</span><br/>
+          <span style={{backgroundColor: 'green', color: 'black'}}>Green puzzles are your lowest priority</span><br/>
+          But they all matter!<br/>
+          <span style={{fontSize: '50px'}}>Your goal is to get the lowest score possible!</span><br/>
+          The bar below your points total tells you how fast your score is increasing. Keep that bar small and green.<br/><br/>
+          The puzzles all have their own set of cryptic clues. We won't tell you exactly how to solve them, but your teammates can: <br/>
+          Anyone assisting in the game should click the 'Manual' button on their own device. That manual contains all of the information you need
+          to solve every puzzle - if you know how to read it. <br/>
+          Good Luck, and we hope to see you on the <Link to='/scores'>Leaderboard!</Link>
+>>>>>>> 2da9584d8f04e5559433d349a53e750c5473f5bf
         </p>
       )}
       <LinkContainer to="/manual">
